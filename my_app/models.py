ae101ac40327c14a9a2387035424f9fef0103ff7
aphiwat-oboun <flim0614759683@gmail.com>
2026-08-03 12:23:52 +0700

    feat: implement student model, migration, and views with index and about templates

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,30 @@
+# pyrefly: ignore [missing-import]
 from django.db import models
+# pyrefly: ignore [missing-import]
+from django.urls import reverse
+# pyrefly: ignore [missing-import]
+from django.contrib import admin
 
-# Create your models here.
+PREFIX_NAME = (
+    ("1", "นาย"),
+    ("2", "นาง"),
+    ("3", "นางสาว"),
+)
+
+# stu_id FirstName LastName
+class Student(models.Model):
+    prefix_name = models.CharField(max_length=10, choices=PREFIX_NAME, default=1)
+    stu_id = models.CharField(max_length=10, unique=True)
+    fname = models.CharField(max_length=50, blank=True)
+    lname = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f'{self.stu_id} {self.get_prefix_name_display()} {self.fname} {self.lname}'
+    
+    def get_absolute_url(self):
+        return reverse('student_detail', kwargs={'pk': self.pk})
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('stu_id', 'get_prefix_name_display', 'fname', 'lname')
+
+admin.site.register(Student, StudentAdmin)
--- my_app/views.py
+++ my_app/views.py
@@ -1,11 +1,14 @@
 # pyrefly: ignore [missing-import]
 from django.shortcuts import render
 import datetime
+from .models import Student
 
 def home(request):
     context = {
         "title": "My Home Page",
     }
+    
+    context["students"] = Student.objects.all().order_by("stu_id")
 
     context["date"] = datetime.datetime.today()
     return render(request, "index.html", context)
