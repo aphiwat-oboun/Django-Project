8839bd225309fe97461fc0fa75412fc17705b2bc
aphiwat-oboun <flim0614759683@gmail.com>
2026-08-10 11:15:08 +0700

    Send Homework

--- my_app/models.py
+++ my_app/models.py
@@ -11,20 +11,39 @@ PREFIX_NAME = (
     ("3", "นางสาว"),
 )
 
+# class Major
+class Major(models.Model):
+    major_name = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f'{self.major_name}'
+    
+    def get_absolute_url(self):
+        return reverse('major_detail', kwargs={'pk': self.pk})
+    
+
 # stu_id FirstName LastName
 class Student(models.Model):
     prefix_name = models.CharField(max_length=10, choices=PREFIX_NAME, default=1)
     stu_id = models.CharField(max_length=10, unique=True)
     fname = models.CharField(max_length=50, blank=True)
     lname = models.CharField(max_length=50, blank=True)
+    major = models.ForeignKey(Major, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
-        return f'{self.stu_id} {self.get_prefix_name_display()} {self.fname} {self.lname}'
+        return f'{self.stu_id} {self.get_prefix_name_display()} {self.fname} {self.lname} {self.major}'
     
     def get_absolute_url(self):
         return reverse('student_detail', kwargs={'pk': self.pk})
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('stu_id', 'get_prefix_name_display', 'fname', 'lname')
+    list_display = ('get_prefix_name_display', 'stu_id', 'fname', 'lname', 'major')
+    ordering = ('stu_id',)
 
 admin.site.register(Student, StudentAdmin)
+
+class MajorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'major_name')
+    ordering = ('id',)
+
+admin.site.register(Major, MajorAdmin)
--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,7 @@
 # pyrefly: ignore [missing-import]
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import datetime
-from .models import Student
+from .models import Student, Major
 
 def home(request):
     context = {
@@ -9,6 +9,7 @@ def home(request):
     }
     
     context["students"] = Student.objects.all().order_by("stu_id")
+    context["majors"] = Major.objects.all().order_by("id")
 
     context["date"] = datetime.datetime.today()
     return render(request, "index.html", context)
@@ -26,4 +27,16 @@ def contact(request):
         "title": "Contact",
     }
     return render(request, "contact.html", context)
+
+def student_detail(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+
+    context = {
+        "title": f"Student: {student.get_prefix_name_display()}{student.fname} {student.lname}",
+        "student": student,
+        "date": datetime.datetime.today(),
+    }
+
+    return render(request, "student_detail.html", context)
+
     
